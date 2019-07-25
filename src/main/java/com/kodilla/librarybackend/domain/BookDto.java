5c9b9894c8029c32e2377ddba6fcedd349f5ba76
aradzikowska12 <aradzikowska12@gmail.com>
2019-07-25 23:20:01 +0200

    task1

---
+++ src/main/java/com/kodilla/librarybackend/domain/BookDto.java
@@ -0,0 +1,79 @@
+package com.kodilla.librarybackend.domain;
+
+import java.util.Objects;
+
+public class BookDto {
+
+    private Long id;
+    private String title;
+    private String author;
+    private Long year;
+    private boolean rented;
+    private String groupId;
+
+    public BookDto() {
+    }
+
+    public BookDto(String title, String author, Long year, boolean rented, String groupId) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.rented = rented;
+        this.groupId = groupId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public Long getYear() {
+        return year;
+    }
+
+    public boolean isRented() {
+        return rented;
+    }
+
+    public String getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(String groupId) {
+        this.groupId = groupId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BookDto bookDto = (BookDto) o;
+        return isRented() == bookDto.isRented() &&
+                getId().equals(bookDto.getId()) &&
+                getTitle().equals(bookDto.getTitle()) &&
+                getAuthor().equals(bookDto.getAuthor()) &&
+                getYear().equals(bookDto.getYear()) &&
+                getGroupId().equals(bookDto.getGroupId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getTitle(), getAuthor(), getYear(), isRented(), getGroupId());
+    }
+
+    @Override
+    public String toString() {
+        return "BookDto{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", year=" + year +
+                '}';
+    }
+}
---
+++ src/main/java/com/kodilla/librarybackend/exceptions/BookNotFoundException.java
@@ -0,0 +1,4 @@
+package com.kodilla.librarybackend.exceptions;
+
+public class BookNotFoundException extends Exception {
+}
