d952b5d9c3e69d8618c4839cbd22c447406b6c9c
aradzikowska12 <aradzikowska12@gmail.com>
2019-07-29 20:20:44 +0200

    task28

--- src/main/java/com/kodilla/librarybackend/controller/CartController.java
+++ src/main/java/com/kodilla/librarybackend/controller/CartController.java
@@ -1,8 +1,9 @@
 package com.kodilla.librarybackend.controller;
 
-import com.kodilla.librarybackend.domain.CartBookAdderDto;
-import com.kodilla.librarybackend.domain.CartBookRemoverDto;
+import com.kodilla.librarybackend.domain.BookDto;
 import com.kodilla.librarybackend.domain.CartDto;
+import com.kodilla.librarybackend.domain.ReservationCreationDto;
+import com.kodilla.librarybackend.domain.ReservationDto;
 import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
@@ -17,18 +18,21 @@ public class CartController {
     }
 
     @RequestMapping(method = RequestMethod.POST, name = "addBookWithSpecifiedIdToSpecifiedCart")
-    public void addBookWithSpecifiedIdToSpecifiedCart (@RequestBody CartBookAdderDto cartBookAdderDto){
-
+    public void addBookWithSpecifiedIdToSpecifiedCart (@RequestBody BookDto bookDto){
+        CartDto cartDto = new CartDto();
+        cartDto.getBookDtoList().add(0,bookDto);
     }
 
     @RequestMapping(method = RequestMethod.DELETE, name = "removeBookWithSpecifiedIdToSpecifiedCart")
-    public void removeBookWithSpecifiedIdToSpecifiedCart (@RequestBody CartBookRemoverDto cartBookRemoverDto){
+    public void removeBookWithSpecifiedIdToSpecifiedCart (@RequestParam Long bookId){
+        CartDto cartDto = new CartDto();
+        cartDto.getBookDtoList().remove(new BookDto());
 
     }
 
     @RequestMapping(method = RequestMethod.PUT , name = "createReservationByCartId", consumes = APPLICATION_JSON_VALUE)
-    public void createReservationByCartId(@RequestParam Long cartId){
-
+    public ReservationCreationDto createReservationByCartId(@RequestParam Long cartId){
+        return new ReservationCreationDto();
     }
 
 
--- src/main/java/com/kodilla/librarybackend/domain/CartDto.java
+++ src/main/java/com/kodilla/librarybackend/domain/CartDto.java
@@ -1,7 +1,21 @@
 package com.kodilla.librarybackend.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CartDto {
 
+    private Long cartId;
+    private List <BookDto> bookDtoList = new ArrayList<>();
+
     public CartDto() {
+    }
+
+    public Long getCartId() {
+        return cartId;
+    }
+
+    public List<BookDto> getBookDtoList() {
+        return bookDtoList;
     }
 }
