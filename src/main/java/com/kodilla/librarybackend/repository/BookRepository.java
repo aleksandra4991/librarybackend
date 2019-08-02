e44d1d96739c08aeabeca1a9c5ebf775738a7420
aradzikowska12 <aradzikowska12@gmail.com>
2019-08-02 20:21:06 +0200

    task17

---
+++ src/main/java/com/kodilla/librarybackend/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package com.kodilla.librarybackend.repository;
+
+import com.kodilla.librarybackend.domain.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
