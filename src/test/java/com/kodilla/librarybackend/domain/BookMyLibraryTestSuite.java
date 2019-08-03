751eb3da75cf9de1b9f5831ce38f6e00c2a04f38
aradzikowska12 <aradzikowska12@gmail.com>
2019-08-03 08:07:32 +0200

    task16

--- src/test/java/com/kodilla/librarybackend/domain/BookMyLibraryTestSuite.java
+++ src/test/java/com/kodilla/librarybackend/domain/BookMyLibraryTestSuite.java
@@ -18,13 +18,13 @@ public class BookMyLibraryTestSuite {
     private BookRepository bookRepository;
 
     @Test
-    public void getAllBooks(){
+    public void getAllBooks() {
 
         //Given
         Genre testGenre = new Genre("Gatunek Testowy");
-        Book book1 = new Book("Tytuł1","Autor1", (long) 1958,"B19876",testGenre);
-        Book book2 = new Book("Tytuł2","Autor2", (long) 1959,"B19877",testGenre);
-        Book book3 = new Book("Tytuł3","Autor3", (long) 1960,"B19878",testGenre);
+        Book book1 = new Book("Tytuł1", "Autor1", (long) 1958, "B19876", testGenre);
+        Book book2 = new Book("Tytuł2", "Autor2", (long) 1959, "B19877", testGenre);
+        Book book3 = new Book("Tytuł3", "Autor3", (long) 1960, "B19878", testGenre);
         int booksCounterBeforeSave = bookRepository.findAll().size();
         bookRepository.save(book1);
         bookRepository.save(book2);
@@ -34,7 +34,161 @@ public class BookMyLibraryTestSuite {
         int booksCounterAfterSave = bookRepository.findAll().size();
 
         //Then
-        Assert.assertEquals(booksCounterBeforeSave + 3,booksCounterAfterSave);
+        Assert.assertEquals(booksCounterBeforeSave + 3, booksCounterAfterSave);
 
     }
+
+    @Test
+    public void testGetAvaiableToRentBooks (){
+
+        //Given
+        Genre testGenre = new Genre("Gatunek Testowy");
+        Book book1 = new Book("Tytuł1", "Autor1", (long) 1958, "B19876",false, testGenre);
+        Book book2 = new Book("Tytuł2", "Autor2", (long) 1959, "B19877",false, testGenre);
+        Book book3 = new Book("Tytuł3", "Autor3", (long) 1960, "B19878",false, testGenre);
+
+        bookRepository.save(book1);
+        bookRepository.save(book2);
+        bookRepository.save(book3);
+
+        //When
+        int numberOfAvaiableBooks = bookRepository.findAll().size();
+        String titleOfBook1 = book1.getTitle();
+        String authorOfBook2 = book2.getAuthor();
+        String signatureOfBook3 = book3.getSignature();
+
+        //Then
+        Assert.assertEquals(3,numberOfAvaiableBooks);
+        Assert.assertEquals("Tytuł1",titleOfBook1);
+        Assert.assertEquals("Autor2",authorOfBook2);
+        Assert.assertNotEquals("B19876",signatureOfBook3);
+
+    }
+
+    @Test
+    public void testGetAlreadyRentedBooks () {
+
+        //Given
+        Genre testGenre = new Genre("Gatunek Testowy");
+        Book book1 = new Book("Tytuł1", "Autor1", (long) 1958, "B19876",true, testGenre);
+        Book book2 = new Book("Tytuł2", "Autor2", (long) 1959, "B19877",true, testGenre);
+        Book book3 = new Book("Tytuł3", "Autor3", (long) 1960, "B19878",true, testGenre);
+
+        bookRepository.save(book1);
+        bookRepository.save(book2);
+        bookRepository.save(book3);
+
+        //When
+        int numberOfRRentedBooks = bookRepository.findAll().size();
+        boolean book11IsRented = book1.isRented();
+        boolean book21IsRented = book2.isRented();
+        Genre genreOfbook1 = book1.getGenre();
+        Genre genreOfbook3 = book3.getGenre();
+
+        Assert.assertNotNull(numberOfRRentedBooks);
+        Assert.assertTrue(book11IsRented==book21IsRented);
+        Assert.assertTrue(genreOfbook1.equals(genreOfbook3));
+
+    }
+
+    @Test
+    public void testGetBooksOfDefiniedAuthor(){
+
+        //Given
+        Genre testGenre = new Genre("Gatunek Testowy");
+        Book book1 = new Book("Tytuł1", "Autor1", (long) 1958, "B19876",true, testGenre);
+        Book book2 = new Book("Tytuł2", "Autor1", (long) 1959, "B19877",false, testGenre);
+        Book book3 = new Book("Tytuł3", "Autor1", (long) 1960, "B19878",true, testGenre);
+
+        bookRepository.save(book1);
+        bookRepository.save(book2);
+        bookRepository.save(book3);
+
+        //When
+        int numberOfBooksOfSpecifiedAuthor = bookRepository.findAll().size();
+        String authorOfFirstBook = book1.getAuthor();
+        String authorOfSecondBook = book2.getAuthor();
+
+        //Then
+        Assert.assertNotEquals(2,numberOfBooksOfSpecifiedAuthor);
+        Assert.assertTrue(authorOfFirstBook.equals(authorOfSecondBook));
+
+    }
+
+    @Test
+    public void testGetBook(){
+
+        //Given
+        Genre testGenre = new Genre("Gatunek Testowy");
+        Book book1 = new Book("Tytuł1", "Autor1", (long) 1958, "B19876",true, testGenre);
+
+        bookRepository.save(book1);
+
+        //When
+        long yearOfFirstBook = book1.getYear();
+
+        //Then
+        Assert.assertNotNull(yearOfFirstBook);
+
+    }
+
+    @Test
+    public void testCreateBook(){
+
+        //Given
+        Genre testGenre = new Genre("Gatunek Testowy");
+        Book book1 = new Book("Tytuł1", "Autor1", (long) 1958, "B19876",true, testGenre);
+
+        //When
+        bookRepository.save(book1);
+        Long bokkId = book1.getId();
+        Book newBook = bookRepository.getOne(bokkId);
+
+        //Then
+        Assert.assertNotNull(newBook);
+
+    }
+
+    @Test
+    public void testUpdateBook(){
+
+        //Given
+        Genre testGenre = new Genre("Gatunek Testowy");
+        Book book1 = new Book("Tytuł1", "Autor1", (long) 1958, "B19876",true, testGenre);
+        bookRepository.save(book1);
+        Long book1Id = book1.getId();
+        Book specifiedBookeToUpdate = bookRepository.getOne(book1Id);
+
+        //When
+        specifiedBookeToUpdate.setRented(false);
+        bookRepository.save(specifiedBookeToUpdate);
+        specifiedBookeToUpdate= bookRepository.getOne(book1Id);
+        boolean updatedIsRentedForSpecifiedBook = specifiedBookeToUpdate.isRented();
+
+        //Then
+        Assert.assertEquals(false,updatedIsRentedForSpecifiedBook);
+    }
+
+    @Test
+    public void testDeleteBook(){
+
+        //Given
+        Genre testGenre = new Genre("Gatunek Testowy");
+        Book book1 = new Book("Tytuł1", "Autor1", (long) 1958, "B19876", testGenre);
+        Book book2 = new Book("Tytuł2", "Autor2", (long) 1959, "B19877", testGenre);
+        Book book3 = new Book("Tytuł3", "Autor3", (long) 1960, "B19878", testGenre);
+        bookRepository.save(book1);
+        bookRepository.save(book2);
+        bookRepository.save(book3);
+
+        //When
+        long bookCounterBeforeDeletion = bookRepository.count();
+        bookRepository.delete(book1);
+        long bookCounterAfterDeletion = bookRepository.count();
+
+
+        //Then
+        Assert.assertNotEquals(bookCounterBeforeDeletion,bookCounterAfterDeletion);
+    }
+
 }
