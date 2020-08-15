46a9c82d77c69c4b5a773009cc559e18e0e877c6
aradzikowska12 <aradzikowska12@gmail.com>
2020-08-15 17:06:36 +0200

    import fixing

--- src/test/java/com/kodilla/librarybackend/domain/BookEntityTestSuite.java
+++ src/test/java/com/kodilla/librarybackend/domain/BookEntityTestSuite.java
@@ -11,7 +11,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 import java.util.ArrayList;
 import java.util.List;
-iport javax.transaction.Transactional;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
